3b50365eeefd80141a255689e46fd75ef865089d
Dmitri Zagidulin <dzagidulin@gmail.com>
2026-05-18 20:34:21 -0400

    Misc prod fixes.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app/[formType]/form/TextEditor/Texteditor.tsx
+++ app/[formType]/form/TextEditor/Texteditor.tsx
@@ -11,7 +11,7 @@ interface TextEditorProps {
   value: any
   onChange: (value: any) => void
 }
-const Clipboard = Quill.import('modules/clipboard')
+const Clipboard = Quill.import('modules/clipboard') as new (...args: any[]) => any
 
 class PlainClipboard extends Clipboard {
   quill: any
--- app/api/export-analytics/route.ts
+++ app/api/export-analytics/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth/next'
 import { getAllUsersAnalyticsData, ExportUserData } from '../../firebase/firestore'
